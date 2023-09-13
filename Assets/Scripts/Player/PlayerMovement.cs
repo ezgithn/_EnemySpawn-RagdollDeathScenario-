368e47df02c668cc9b21b0256ca6964ae55527c4
ezgithn <108123991+ezgithn@users.noreply.github.com>
2023-09-13 22:50:43 +0300

    ScriptSets.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -20,8 +20,8 @@ public class PlayerMovement : MonoBehaviour
     private void HandleMovementInput()
     {
        
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
 
        
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
