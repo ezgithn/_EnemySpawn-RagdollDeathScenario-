74ee367ade0f057e1b7508eda91d1104c7c1098f
ezgithn <108123991+ezgithn@users.noreply.github.com>
2023-09-13 22:42:51 +0300

    RagdollSets.

---
+++ Assets/Scripts/Enemy/EnemyCollision.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyCollision : MonoBehaviour
+{
+    private RagdollController ragdollController;
+
+    private void Start()
+    {
+        ragdollController = GetComponent<RagdollController>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Karaktere çarpışma algılandığında Ragdoll'u etkinleştir
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ragdollController.SetRagdollEnabled(true);
+        }
+    }
+}
+
+internal class RagdollController
+{
+    public void SetRagdollEnabled(bool b)
+    {
+        throw new System.NotImplementedException();
+    }
+}
