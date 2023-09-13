7e37b9e25c53f163e28d646112c4997169b4ff67
ezgithn <108123991+ezgithn@users.noreply.github.com>
2023-09-13 22:45:37 +0300

    PlyrMove.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody rb;
+    public float moveSpeed = 5.0f;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        HandleMovementInput();
+    }
+
+    private void HandleMovementInput()
+    {
+        // Kullanıcının girdi verilerini al
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Hareket vektörünü oluştur
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
+
+        // Hareketi uygula
+        rb.MovePosition(transform.position + movement);
     }
 }
