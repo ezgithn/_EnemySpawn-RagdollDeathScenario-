8375893ec3f0ef2f3bb7cdcebad717b1c899be5f
ezgithn <108123991+ezgithn@users.noreply.github.com>
2023-09-16 11:03:30 +0300

    AnimatorSets.

--- Assets/Scripts/Player/PlayerAnimControl.cs
+++ Assets/Scripts/Player/PlayerAnimControl.cs
@@ -4,5 +4,46 @@ using UnityEngine;
 
 public class PlayerAnimControl : MonoBehaviour
 {
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     
+    public void PlayIdleAnimation()
+    {
+        animator.SetTrigger("Idle");
+    }
+    
+    public void PlayRunAnimation()
+    {
+        animator.SetTrigger("Run");
+    }
+
+    public void PlayCrouchingAnimation()
+    {
+        animator.SetTrigger("Crouch");
+    }
+
+    public void PlayHighSpinAttackAnimation()
+    {
+        animator.SetTrigger("HighSpinAttack");
+    }
+
+    public void PlaySlashAnimation()
+    {
+        animator.SetTrigger("Slash");
+    }
+
+    public void PlayDeathAnimation()
+    {
+        animator.SetTrigger("Death");
+    }
+
+    // Animasyonların sona erip etkisiz hale geldiğini bildiren bir geri arama yöntemi!!!
+    public void AnimationComplete()
+    {
+        
+    }
 }
