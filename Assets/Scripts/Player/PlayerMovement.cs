a72269b8f6905915ae8a30ee130d05b47dff1c56
ezgithn <108123991+ezgithn@users.noreply.github.com>
2023-09-16 15:24:58 +0300

    Update PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,31 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody rb;
+    private Rigidbody _rb;
+    private Animator _animator;
+    
     public float moveSpeed = 5.0f;
+    public float rotationSpeed = 100.0f;
 
+    
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         HandleMovementInput();
+        HandleActions();
     }
 
     private void HandleMovementInput()
     {
-       
-        var horizontalInput = Input.GetAxis("Horizontal");
-        var verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-       
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        movement.Normalize();
 
-      
-        rb.MovePosition(transform.position + movement);
+        _rb.velocity = movement * moveSpeed;
+
+        // Karakterin yönü.
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        
+        if (movement.z > 0f)
+        {
+            _animator.SetBool("IsRunning", true);
+        }
+        else
+        {
+            _animator.SetBool("IsRunning", false);
+        }
+        
+    }
+
+    private void HandleActions()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            _animator.SetTrigger("Crouch");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _animator.SetTrigger("Slash");
+        }
+
+        if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.Q))
+        {
+            _animator.SetTrigger("HighSpinAttack");
+        }
+    }
+
+    public void Die()
+    {
+        // Düşmanla temas edildiğinde çağrılacak..
+        _animator.SetTrigger("Death");
+        // Ölüm animasyonunun tamamlanmasından sonra ragdoll'u etkinleştirmek
+        // için Player Animation scriptindeki AnimationComplete kısmını kullan!!!
     }
 }
