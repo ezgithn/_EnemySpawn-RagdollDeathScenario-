ad9afc26adb223b5fb2222d6a61efa0074b9b9a0
ezgithn <108123991+ezgithn@users.noreply.github.com>
2023-09-22 01:12:35 +0300

    Camera-MouseUpdt.

--- Assets/Scripts/Camera/Camera.cs
+++ Assets/Scripts/Camera/Camera.cs
@@ -9,9 +9,15 @@ public class Camera : MonoBehaviour
 {
     public Transform target; 
     public Vector3 offset = new Vector3(0f, 3f, -5f); 
-    public float smoothSpeed = 5f; 
+    public float smoothSpeed = 5f;
+    public static Camera main { get; set; }
 
     
+    private void Awake()
+    {
+        main = this;
+    }
+
     private void LateUpdate()
     {
         if (!target)
@@ -24,5 +30,11 @@ public class Camera : MonoBehaviour
         
         transform.LookAt(target);
     }
-   
+
+
+    public static Vector3 ScreenToWorldPoint(Vector3 vector3)
+    {
+        throw new System.NotImplementedException();
+    }
+    
 }
--- Assets/Scripts/Player/PlayerMouseControl.cs
+++ Assets/Scripts/Player/PlayerMouseControl.cs
@@ -2,25 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerMouseControl : MonoBehaviour
 {
     public float rotationSpeed = 100f;
     
-    [SerializeField] public Camera playerCamera; 
+    [SerializeField] private Camera playerCamera; 
 
+    
+    void Start()
+    {
+        playerCamera = Camera.main;
+        
+    }
+    
     private void Update()
     {
         RotateCharacterWithMouse();
     }
+    
 
     private void RotateCharacterWithMouse()
     {
         Vector3 mousePosition = Input.mousePosition;
-        var position = transform.position;
-        Vector3 worldMousePosition = playerCamera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, position.y));
+        var position1 = transform.position;
+        Vector3 position = new Vector3(position1.x, 0, position1.z);
+        Vector3 worldMousePosition = Camera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, transform.position.y));
         Vector3 lookDirection = worldMousePosition - position;
         lookDirection.y = 0;
+        
+        // transform.LookAt(transform.position + lookDirection, Vector3.up);
 
         if (lookDirection != Vector3.zero)
         {
@@ -28,4 +38,5 @@ public class PlayerMouseControl : MonoBehaviour
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
     }
+    
 }
