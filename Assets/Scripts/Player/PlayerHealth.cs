6b68fd8ff3da2631da393b34b47f6a5f6a19603c
ezgithn <108123991+ezgithn@users.noreply.github.com>
2023-10-04 15:14:00 +0300

    Update PlayerHealth.cs

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -6,29 +6,57 @@ public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 5; 
     private int _currentHealth; 
+    public int currentHealth => _currentHealth;
+    private Rigidbody[] ragdollRigidbodies;
 
     private void Start()
     {
         _currentHealth = maxHealth;
+        ragdollRigidbodies = GetComponentsInChildren<Rigidbody>();
+        DisableRagdoll();
     }
     
-    // void Update()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.Q))
-    //     {
-    //         TakeDamage(1);
-    //     }
-    // }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TakeDamage(1);
+        }
+    }
     
     public void TakeDamage(int damageAmount)
     {
         _currentHealth -= damageAmount;
 
-        //karakter ölürse oyunu yeniden başlatmak gibi komutları buraya eklicem
+        //karakter ölürse oyunu yeniden başlatmak gibi komutları buraya eklicemm
         
-        if (_currentHealth <= 0)
+        if (currentHealth <= 0)
         {
-            gameObject.SetActive(false);
+            Die();
         }
     }
+    
+    private void Die()
+    {
+        EnableRagdoll();
+    }
+    
+    private void EnableRagdoll()
+    {
+        foreach (var rigidbody in ragdollRigidbodies)
+        {
+            rigidbody.isKinematic = false;
+            rigidbody.useGravity = true;
+        }
+    }
+    
+    private void DisableRagdoll()
+    {
+        foreach (var rigidbody in ragdollRigidbodies)
+        {
+            rigidbody.isKinematic = true;
+            rigidbody.useGravity = false;
+        }
+    }
+    
 }
